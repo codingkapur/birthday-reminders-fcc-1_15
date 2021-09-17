16e684d849a02ed43f42daf0f46472ef1c89d2ce
Raghav <codingkapur@gmail.com>
2021-09-17 14:20:18 +0530

    fix db.json and update birthday data by retrieving data from db.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState, useEffect} from 'react'
+
+
 
 function App() {
+
+  const [birthdays, updateBirthdays]= useState([])
+
+  useEffect(()=> {
+    const getBirthdays = async () =>{ 
+      const data = await fetchBirthdays()
+      updateBirthdays(data)
+    } 
+    getBirthdays()
+  }, [])
+
+  const fetchBirthdays = async () => {
+    const res = await fetch('http://localhost:5000/birthdays')
+    const data = await res.json();
+    return data
+  }
+
   return (
    <h1>Hello React</h1>
   );
